aca5d4895df0230c1a23840865b9dd41a88d3b77
Jaden Stanshall <jaden.uoft@gmail.com>
2024-02-27 23:12:39 -0500

    initialize project

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+// replace all this with new code, just copy and paste from arduino ide
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
